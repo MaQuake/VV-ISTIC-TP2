2b5668640ddea3d600a29c3016ed9ea99fb0c278
Aymerick <aymerick.leborgne@etudiant.univ-rennes1.fr>
2023-12-11 08:09:51 +0100

    TP2 add Exercice 5

--- code/javaparser-starter/src/main/java/fr/istic/vv/PublicElementsPrinter.java
+++ code/javaparser-starter/src/main/java/fr/istic/vv/PublicElementsPrinter.java
@@ -3,16 +3,25 @@ package fr.istic.vv;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.PackageDeclaration;
 import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.expr.SwitchExpr;
+import com.github.javaparser.ast.nodeTypes.SwitchNode;
+import com.github.javaparser.ast.stmt.*;
 import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
 import com.github.javaparser.ast.body.MethodDeclaration;
 
+import java.lang.invoke.SwitchPoint;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 // This class visits a compilation unit and
 // prints all public enum, classes or interfaces along with their public methods
 public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
+
+    private Map<String, Integer> ccValuesMap = new HashMap<>();
 
     private String currentPackageName;
     private String currentClassName;
@@ -23,6 +32,7 @@ public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
         for(TypeDeclaration<?> type : unit.getTypes()) {
             type.accept(this, null);
         }
+        generateHistogram();
     }
 
     public void visitTypeDeclaration(TypeDeclaration<?> declaration, Void arg) {
@@ -61,9 +71,69 @@ public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
     }
     @Override
     public void visit(MethodDeclaration declaration, Void arg) {
-        if(!declaration.isPublic()) return;
-        //System.out.println("  " + declaration.getDeclarationAsString(true, true));
+        //if (!declaration.isPublic()) return;
+
+        String packageName = currentPackageName;
+        String declaringClassName = currentClassName;
+        String methodName = declaration.getNameAsString();
+        List<Parameter> parameters = declaration.getParameters();
+
+        System.out.println("Package: " + packageName);
+        System.out.println("Declaring Class: " + declaringClassName);
+        System.out.println("Method Name: " + methodName);
+
+        // Print parameter types
+        if (!parameters.isEmpty()) {
+            System.out.print("Parameters: ");
+            for (Parameter parameter : parameters) {
+                System.out.print(parameter.getType() + " " + parameter.getNameAsString() + ", ");
+            }
+            System.out.println();
+        }
+
+        int cc = calculateCyclomaticComplexity(declaration);
+        System.out.println("Cyclomatic Complexity: " + cc);
+        System.out.println();
     }
+
+
+    /**
+     * Calculate cyclomatic complexity base on McCabe method
+     * @param method
+     * @return
+     */
+    private int calculateCyclomaticComplexity(MethodDeclaration method) {
+        // Count the number of predicate nodes (conditionals)
+        int predicateNodes = method.getNodesByType(IfStmt.class).size() +
+                method.getNodesByType(WhileStmt.class).size() +
+                method.getNodesByType(ForStmt.class).size() +
+                method.getNodesByType(ForEachStmt.class).size() +
+                method.getNodesByType(SwitchEntry.class).size();
+
+        ccValuesMap.put(method.getNameAsString(), predicateNodes);
+
+        // Simplified calculation of Cyclomatic Complexity: v(G) = number of predicate nodes + 1
+        return predicateNodes + 1;
+    }
+
+    /**
+     * Generate histogram based on collected Cyclomatic Complexity values
+     */
+    private void generateHistogram() {
+        System.out.println("Cyclomatic Complexity Histogram:");
+
+        // Group CC values by count
+        Map<Integer, Long> ccCounts = ccValuesMap.values().stream()
+                .collect(Collectors.groupingBy(cc -> cc, Collectors.counting()));
+
+        // Print histogram
+        ccCounts.forEach((cc, count) -> {
+            System.out.println("CC=" + cc + ": " + "*".repeat(count.intValue()));
+        });
+    }
+
+
+
 
     private boolean hasGetter(FieldDeclaration fieldDeclaration) {
         String fieldName = fieldDeclaration.getVariables().get(0).getNameAsString();
